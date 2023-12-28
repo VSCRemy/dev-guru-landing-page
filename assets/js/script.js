b27ed411580bb077aa54c04f941f2f9cd3b2ffbf
VSCRemy <126096029+VSCRemy@users.noreply.github.com>
2023-12-27 21:35:10 -0700

    hero section

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,3 +29,18 @@ const closeNavbar = function () {
 };
 
 addEventOnElement(navLinks, "click", closeNavbar);
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+const activeElem = function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+};
+
+addEventOnElem(window, "scroll", activeElem);
