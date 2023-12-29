520425b98201e95cc431d3061c15f887cb61251a
VSCRemy <126096029+VSCRemy@users.noreply.github.com>
2023-12-28 18:50:18 -0700

    final

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const addEventOnElement = function (elem, type, callback) {
+const addEventOnElem = function (elem, type, callback) {
   if (elem.length > 1) {
     for (let i = 0; i < elem.length; i++) {
       elem[i].addEventListener(type, callback);
@@ -10,7 +10,6 @@ const addEventOnElement = function (elem, type, callback) {
   }
 };
 
-// Navbar toggle
 const navbar = document.querySelector("[data-navbar]");
 const navTogglers = document.querySelectorAll("[data-nav-toggler]");
 const navLinks = document.querySelectorAll("[data-nav-link]");
@@ -21,14 +20,14 @@ const toggleNavbar = function () {
   overlay.classList.toggle("active");
 };
 
-addEventOnElement(navTogglers, "click", toggleNavbar);
+addEventOnElem(navTogglers, "click", toggleNavbar);
 
 const closeNavbar = function () {
   navbar.classList.remove("active");
   overlay.classList.remove("active");
 };
 
-addEventOnElement(navLinks, "click", closeNavbar);
+addEventOnElem(navLinks, "click", closeNavbar);
 
 const header = document.querySelector("[data-header]");
 const backTopBtn = document.querySelector("[data-back-top-btn]");
